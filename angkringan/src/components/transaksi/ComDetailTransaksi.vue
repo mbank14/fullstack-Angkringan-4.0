a9ab826f8cefe1cee34ca7fc692621ae225a0e94
Jembar <jembar.ashofa01@gmail.com>
2020-11-10 17:46:57 +0700

    detail transaksi tampilan

---
+++ angkringan/src/components/transaksi/ComDetailTransaksi.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>ini detail transaksi</h1> 
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailTransaksion: [
+                {
+                    id: "1231",
+                    items: [
+                        {
+                            id: "#31212",
+                            nama: "sarung",
+                            deskripsi: "sarung gajah duduk",
+                            harga: 1000,
+                            qyt:3,
+                            
+                        },
+                        {
+                            id: "#31212",
+                            nama: "sarung",
+                            deskripsi: "sarung gajah duduk",
+                            harga: 1000,
+                            qyt:3,
+                            
+                        },
+                        {
+                            id: "#31212",
+                            nama: "sarung",
+                            deskripsi: "sarung gajah duduk",
+                            harga: 1000,
+                            qyt:3,
+                            
+                        },
+                        {
+                            id: "#31212",
+                            nama: "sarung",
+                            deskripsi: "sarung gajah duduk",
+                            harga: 1000,
+                            qyt:3,
+                            
+                        },
+                    ]
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
